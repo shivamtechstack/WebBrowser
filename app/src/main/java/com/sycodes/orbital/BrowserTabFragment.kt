739c15959fc8f0b54a7b865964b14e2c0bc30f02
shivamtechstack <devshivamyadav1604@gmail.com>
2025-03-29 23:40:15 +0530

    - Implement toolbar menu popup with icons and replace icons.

--- app/src/main/java/com/sycodes/orbital/BrowserTabFragment.kt
+++ app/src/main/java/com/sycodes/orbital/BrowserTabFragment.kt
@@ -1,12 +1,17 @@
 package com.sycodes.orbital
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.PopupMenu
+import com.sycodes.orbital.databinding.FragmentBrowserTabBinding
 
 class BrowserTabFragment : Fragment() {
+    private lateinit var binding: FragmentBrowserTabBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +22,22 @@ class BrowserTabFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_browser_tab, container, false)
+        binding = FragmentBrowserTabBinding.inflate(inflater, container, false)
+
+        binding.toolbarMenuIcon.setOnClickListener { view ->
+           showPopupMenu(view)
+        }
+
+        return binding.root
+    }
+
+    private fun showPopupMenu(view1: View) {
+        val popupMenu = PopupMenu(requireContext(), view1)
+        popupMenu.menuInflater.inflate(R.menu.toolbarmenu, popupMenu.menu)
+
+        popupMenu.setForceShowIcon(true)
+        popupMenu.show()
+
     }
 
 }
---
+++ app/src/main/java/com/sycodes/orbital/menus/ToolBarPopUpMenu.kt
@@ -0,0 +1,7 @@
+package com.sycodes.orbital.menus
+
+import androidx.appcompat.widget.PopupMenu
+import androidx.core.content.ContentProviderCompat.requireContext
+
+class ToolBarPopUpMenu {
+}
